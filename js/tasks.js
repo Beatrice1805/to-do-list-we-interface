7966ec921bcef810e2228669f04d3047bb4d8c61
Beatrice <oprea.beatrice@yahoo.com>
2020-02-26 07:01:56 +0200

    displaying tasks from api

--- js/tasks.js
+++ js/tasks.js
@@ -2,29 +2,50 @@ window.ToDoList = {
 
     API_BASE_URL: "http://localhost:8081/tasks",
 
-
     getTasks: function () {
-
-        $.ajax({
-
+        $.ajax( {
             url: ToDoList.API_BASE_URL,
             method: "GET"
 
         }).done(function (response) {
             console.log(response);
-        })
 
-        },
-    getTaskRow: function(task) {
+            ToDoList.displayTasks(JSON.parse(response));
+        })
+    },
+    getTaskRow: function (task) {
+        //spread operator (...)
+        let formattedDeadline =
+            new Date(...task.deadline).toLocaleDateString("ro");
+        //ternary operator
+
+        let  checkedAttribute = task.done? " checked": "";
+
+        //same result as with the ternary operator above
+//if (task.done){
+//checkedAttribute ="checked";
+  //  }else{
+     //   checkedAttribute = "";
+//}
         return `<tr>
             <td>${task.description}</td>
-            <td>${task.deadline}</td>
-            <td><input type="checkbox" data-id=${task.id} class="mark-done"/></td>
+            <td>${formattedDeadline}</td>
+            <td><input type="checkbox" data-id=${task.id} class="mark-done" ${checkedAttribute}/></td>
             <td><a href="#" data-id=${task.id} class="delete-task">
                 <i class="fas fa-trash-alt"></i>
             </a></td>
         </tr>`
-    }
+    },
+
+    displayTasks: function (tasks) {
+        //weak-typed (javascript) vs strong tyoed (java)
+        var tableBody = '';
+
+        tasks.forEach(task => tableBody += ToDoList.getTaskRow(task));
+
+        $("#tasks-table tbody").html(tableBody);
+
+}
     };
 
 ToDoList.getTasks();
